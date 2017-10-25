243f1ba251103549ee8a59b8e503ae22dfe8e987
YexChen <76753500@qq.com>
2017-10-25 11:16:51 +0800

    初始化了下数据库，还有好多要做啊，加油

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Zhihu-rebuild
 知乎网站重构(利用express部署)
 
+#关于github的使用
+先commit,再pull，再push
+
+#
+
 #技术栈
 数据引擎:NodeJS
-数据库:MongoDB
+数据库:MongoDB zhihu
 web服务器框架:express
 模板渲染引擎:ejs
 后端包管理:npm
@@ -13,3 +18,19 @@ web服务器框架:express
 实现用户登录，注册，更改头像，个人简介，个人提问，回答，查看发表的文章，收藏，关注等功能
 实现发表提问，回答等功能
 后续还有功能，待添加
+
+
+#users--用户表的设计
+users
+用户表
+id 登陆id
+username 用户名
+nickname 用户昵称
+password 密码
+question 问的问题
+answer 回答的问题
+ article 写的文章
+Star 收藏的文章
+Fave 可能喜欢的内容
+created_time 创建时间
+updated_time 最后登录时间
--- app.js
+++ app.js
@@ -5,14 +5,20 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+//使用数据库
+let db = require(__dirname+"/config/db.js");
+
+//根路由器
+let router = require('./routes/router');
+
+
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +28,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
